03ce22ed070ba5b8e93c2ddeb1f426315cbbcafa
pooja199893 <poojacrane15111998@gmail.com>
2024-01-28 13:25:59 +0530

    updates

---
+++ DAY1/day1.js
@@ -0,0 +1,50 @@
+console.log('abcd')
+console.log(typeof('abcd'))
+console.log("qwe")
+console.log(typeof("qwe"))
+console.log(`az`)
+console.log(typeof(`az`))
+
+console.log(`az`,typeof('jskery'),"uwEIRRG")
+console.log(2+2)
+
+console.log(1234,typeof(1234))
+console.log(1+1)
+console.log('1'+"1")
+console.log(true,typeof(true))
+console.log(false,typeof(false))
+console.log("true",typeof(true))
+
+//variable
+var insitutename="awdiz"
+var age=12;
+var isopen=true
+console.log(insitutename,age,isopen)
+
+var values1=12;
+var values2=5
+console.log(values1+values2)
+console.log(values1-values2)
+console.log(values1*values2)
+console.log(values1/values2)
+
+var abc=23
+console.log(abc)
+console.log(abc)
+console.log(abc)
+console.log(abc)
+console.log(abc)
+
+const abc=1234;
+abc=12
+console.log(abc)
+
+
+let abc=1234;
+abc=434;
+console.log(abc)
+
+
+
+
+
