499a18c122f7f3e445e69a0411775fe0259efa85
pooja199893 <poojacrane15111998@gmail.com>
2024-02-01 11:52:19 +0530

    updates

---
+++ DAY2/day2.js
@@ -0,0 +1,19 @@
+{
+    let myName="Awdiz";
+    console.log(myName)
+}
+{
+   const mySurname= "xyz";
+   console.log(mySurname)
+}
+console.log(myName)
+console.log(mySurname)
+
+console.log(3<2)
+console.log(3>2)
+console.log(3<=2)
+console.log(3>=2)
+console.log(3==2)
+console.log(3=="2")
+console.log(3===2)
+console.log(3==="2")
