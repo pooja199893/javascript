c7cc83124a2a6046281e1c0f851f8822e8c29ce2
pooja199893 <poojacrane15111998@gmail.com>
2024-02-02 12:47:35 +0530

    update

---
+++ DAY3/day3.js
@@ -0,0 +1,28 @@
+if(3>30){
+    console.log("condition is true.")
+}else{
+    console.log("condition is false.")
+}
+
+var v1=40;
+var v2=30;
+var v3=43;
+if(v1>v2)  {
+    console.log("v 1 is bigger than v 2")
+} else if(v1>v3) {
+    console.log("v 1 is bigger than v 3")
+} else {
+    console.log("v 1 is lesser than v 2 and v3 ")
+}
+
+var age=19;
+if (age == 18){
+    console.log("you allowed for LL only")
+
+} else if(age > 18 && age <= 80){
+    console.log("you alloed for L")
+} else {
+    console.log(" you not allowed for L")
+}
+
+
